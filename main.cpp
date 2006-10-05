5368399ba521ac6210f1901093c97fce786b4963
meonimarco <meonimarco@babac969-fc1e-0410-b22c-d73624067e4c>
2006-10-05 09:37:10 +0000

    Levato suuuuka

--- main.cpp
+++ main.cpp
@@ -14,9 +14,6 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-//SUUUKA
-
-
 #include <cvaux.h>  // include core library interface
 #include <highgui.h> // include GUI library interface
 #include <stdio.h>
