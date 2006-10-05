93de123ffe6cb6ce7b2ba5a6c5757ea41e300da1
martorana.nicola <martorana.nicola@babac969-fc1e-0410-b22c-d73624067e4c>
2006-10-05 09:35:28 +0000

    Suuka

--- main.cpp
+++ main.cpp
@@ -13,6 +13,10 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+
+//SUUUKA
+
+
 #include <cvaux.h>  // include core library interface
 #include <highgui.h> // include GUI library interface
 #include <stdio.h>
