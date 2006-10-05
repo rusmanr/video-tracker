ae7a5b65a01022acfc129400708826db6a7f488b
martorana.nicola <martorana.nicola@babac969-fc1e-0410-b22c-d73624067e4c>
2006-10-05 18:45:13 +0000

    ecco il file con le WX

--- getFrames.cpp
+++ getFrames.cpp
@@ -8,54 +8,56 @@
 int main(int argc, char *argv[]) {
 	CvCapture* capture = cvCaptureFromAVI("spheres1.avi");
 
-  if( !capture ) {
-    fprintf( stderr, "ERROR: capture is NULL \n" );
-    getchar();
-    return -1;
-  }
+	if( !capture ) {
+		fprintf( stderr, "ERROR: capture is NULL \n" );
+		getchar();
+		return -1;
+	}
 
-  // Create a window in which the captured images will be presented
-  cvNamedWindow( "mywindow", CV_WINDOW_AUTOSIZE );
+	// Create a window in which the captured images will be presented
+	cvNamedWindow( "mywindow", CV_WINDOW_AUTOSIZE );
 
-  // Show the image captured from the camera in the window and repeat
-  int k=0;
-  char filesave[50];
-  //bool flag = false;
-  while( true ) {
-				k++;
-				// Get one frame
-				IplImage* frame = cvQueryFrame( capture );
-				if( !frame ) {
-						fprintf( stderr, "There is no frame to get. Press Any Key\n" );
-						getchar();
-						//flag=true;
-						break;
-						}			
-				IplImage* img = 0;
-				cvShowImage( "mywindow", frame );
-				img=cvRetrieveFrame(capture);
+	// Show the image captured from the camera in the window and repeat
+	int k=0;
+	char filesave[50];
+	//bool flag = false;
+	while( true ) {
+		k++;
+		// Get one frame
+		IplImage* frame = cvQueryFrame( capture );
+		if( !frame ) {
+			fprintf( stderr, "There is no frame to get. Press Any Key\n" );
+			getchar();
+			//flag=true;
+			break;
+			}			
+		IplImage* img = 0;
+		cvShowImage( "mywindow", frame );
+		img=cvRetrieveFrame(capture);
 
 
 //----------------------CODICE CHE USA LE WX---------------------------
 
-				if (! wxDirExists("./data")){
-					wxMkdir("./data");
-				}
+		if (! wxDirExists("./data")){
+			wxMkdir("./data");
+			}
 
-//---------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 
-				sprintf(filesave,"./data/immagine%d.jpg",k);
-				if(!cvSaveImage(filesave,img)) printf("Could not save the image\n");
+		sprintf(filesave,"./data/immagine%d.jpg",k);
 				
-				// Do not release the frame!
-				//If ESC key pressed, Key=0x10001B under OpenCV 0.9.7(linux version),
-				//remove higher bits using AND operator
-				if( (cvWaitKey(10) & 255) == 27 ) break;
-				}
+		if(!cvSaveImage(filesave,img)) printf("Could not save the image\n");
+				
+		// Do not release the frame!
+		//If ESC key pressed, Key=0x10001B under OpenCV 0.9.7(linux version),
+		//remove higher bits using AND operator
+		if( (cvWaitKey(10) & 255) == 27 ) break;
+	
+		}
 
-  // Release the capture device housekeeping
-  cvReleaseCapture( &capture );
-  cvDestroyWindow( "mywindow" );
-  //getchar();
-  return 0;
+	// Release the capture device housekeeping
+	cvReleaseCapture( &capture );
+	cvDestroyWindow( "mywindow" );
+	//getchar();
+	return 0;
 }
